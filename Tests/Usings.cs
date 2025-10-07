1c903cefad488d46cec4da1ec85bc9eda33d01b9
Richard Garcia <code.richardy@gmail.com>
2025-10-06 23:05:59 -0300

    this commit adds global usings for the test suite

---
+++ Tests/Usings.cs
@@ -0,0 +1,28 @@
+/* global using for System namespaces here */
+
+global using System.Net;
+global using System.Net.Http.Headers;
+global using System.Net.Http.Json;
+
+/* global using for Microsoft namespaces here */
+
+global using Microsoft.AspNetCore.Mvc.Testing;
+global using Microsoft.Extensions.DependencyInjection;
+
+/* global using for Vinder namespaces here */
+
+global using Vinder.Sample.WebApi;
+global using Vinder.Sample.TestSuite.Fixtures;
+
+global using Vinder.Internal.Essentials.Patterns;
+
+global using Vinder.IdentityProvider.Sdk.Extensions;
+global using Vinder.IdentityProvider.Sdk.Contracts.Payloads.Identity;
+
+/* global usings for third-party namespaces here */
+
+global using MongoDB.Driver;
+global using DotNet.Testcontainers.Builders;
+global using DotNet.Testcontainers.Containers;
+global using AutoFixture;
+global using Moq;
